67a1ead4eee96be19cdf77831a786e182624c9ba
Omar Essam <omar161157@eng.psu.edu.eg>
2023-10-31 01:29:17 +0200

    new commit

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.index("object") : str.index("programming") + 11] + " " + str[str.index("with") : str.index("with") + 4]+ " " +str[:6]
+str = str[39 : 39+27] + " " + str[107 :111]+ " " +str[:6]
 print(str)

e4c3c8db76df34256f7262e3c6752a0f0802a55f
Omar Essam <omar161157@eng.psu.edu.eg>
2023-11-01 02:29:05 +0200

    new commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the head of the list{
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fp = list->next->next, *sp = list->next;
+	int res = 0;
+
+
+	while (sp != NULL)
+	{
+		if (sp == fp)
+		{
+			res = 1;
+			break;
+		}
+
+		fp = fp->next->next, sp = sp->next;
+
+	}
+
+	return (res);
+}

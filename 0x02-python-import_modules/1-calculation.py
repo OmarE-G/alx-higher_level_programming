3d0b34fa2304ea0cb3828ba9f55c163478e8fa18
Omar Essam <omar161157@eng.psu.edu.eg>
2023-11-03 04:22:33 +0200

    new commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,12 @@
-import calculator_1 as calc
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
 if __name__ != "__main__":
     exit()
 
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, calc.add(a, b)))
-print("{} - {} = {}".format(a, b, calc.sub(a, b)))
-print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-print("{} / {} = {}".format(a, b, calc.div(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))

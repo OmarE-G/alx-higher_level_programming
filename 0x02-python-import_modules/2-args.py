1b6705490e09b9cb75b0aac0ec89c9d7af83f6e8
Omar Essam <omar161157@eng.psu.edu.eg>
2023-11-03 04:44:09 +0200

    new commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,8 @@ from sys import argv
 if __name__ != "__main__":
     exit()
 
-print("{} argument".format(len(argv) - 1) + 
+print("{} argument".format(len(argv) - 1) +
       ('s' if len(argv) != 2 else '') + ('.' if len(argv) == 1 else ':'))
 
 for i in range(1, len(argv)):
     print("{}: {}".format(i, argv[i]))
-    

9709ca7608ff5b52e045047ce0302547458a9e83
Omar Essam <omar161157@eng.psu.edu.eg>
2023-11-03 17:58:36 +0200

    new commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ != "__main__":
 if len(argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
-    
+
 a, b = int(argv[1]), int(argv[3])
 op = argv[2]
 
@@ -25,4 +25,3 @@ elif op == '/':
     res = div(a, b)
 
 print("{} {} {} = {}".format(a, op, b, res))
-

a3b5a4f7084e7706a9d4f2e2ab63ee15c747f7e8
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-09 06:30:24 +0200

    New commit

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     l = []
-    for i in range (list_length):
+    for i in range(list_length):
         x = 0
         try:
             x = my_list_1[i]/my_list_2[i]

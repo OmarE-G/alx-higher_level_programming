448f0b87c8da0ef770fa2ddd055a3d9ddb81e862
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-09 05:25:39 +0200

    New commit

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,6 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             x = 0
         except IndexError:
             print("out of range")
+            continue
         finally:
             list_length[i] = x
     return list_length          

1025d10b3c53ef0a569975111237c0d98192271c
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-09 06:19:20 +0200

    New commit

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             x = my_list_1[i]/my_list_2[i]
         except ZeroDivisionError:
-            print("divison by zero")
+            print("divison by 0")
         except TypeError:
             print("wrong type")
         except IndexError:

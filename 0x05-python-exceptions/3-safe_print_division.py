f7533b4d7653a60a0bad4372edf64b4515f04fbb
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-09 04:57:41 +0200

    New commit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print("Inside result: {:d}".format(a/b))
+        print("Inside result: {}".format(a/b))
     return a/b
         
     

8e97a29ba090d7f562d153056652d28bb796ad64
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-09 04:16:38 +0200

    New commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     c = 0
     for i in range(x):
-        try:  
+        try:
             print(my_list[i], end="")
-            c+=1
+            c += 1
         except IndexError:
             continue
-    print('\n')
-    return c
+        return c

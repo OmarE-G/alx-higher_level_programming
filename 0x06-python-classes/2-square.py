27030a3149565791bccc427db7ad8ebf49451526
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-10 21:02:22 +0200

    New commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@
 class Square:
     """Empty class that defines a square"""
     def __init__(self, size=0):
-        if size is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")

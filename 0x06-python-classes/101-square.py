bafbc1a1c11a1376dbabdee5594a37193ccaebc9
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-11 04:04:41 +0200

    New commit

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""module
+"""
+
+
+class Square:
+    """Empty class that defines a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def __str__(self):
+        str = '\n' * self.position[1] * (self.size != 0)
+        for i in range(self.size):
+            str += self.position[0] * ' ' + '#' * self.size + '\n'
+        return str + '\n' * int(self.size == 0)
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            print('\n' * self.position[1], end='')
+            for i in range(self.__size):
+                print(' ' * self.position[0] + '#' * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, val):
+        if type(val) is not int:
+            raise TypeError("size must be an integer")
+        if val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = val
+
+    @position.setter
+    def position(self, value):
+        if not (type(value) is tuple
+                and len(value) == 2
+                and all((type(i) is int and i >= 0) for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+
+print(Square(0, (0, 5)))

df602fb24f5a4edea773360eded611bd734f9859
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-11 20:22:37 +0200

    New commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""add integer module
+"""
+
 
 def add_integer(a, b=98):
+    """function to add 2 integers"""
     if not isinstance(a, (int, float)):
         raise (TypeError("a must be an integer"))
     if not isinstance(b, (int, float)):

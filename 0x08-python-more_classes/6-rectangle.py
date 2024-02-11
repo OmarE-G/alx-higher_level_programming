1bc742be0352f995a1af1a8d662d2f8f4180d986
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-11 06:17:28 +0200

    New commit

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,11 +10,11 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     def __del__(self):
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
 
     def __str__(self):
         str = ""

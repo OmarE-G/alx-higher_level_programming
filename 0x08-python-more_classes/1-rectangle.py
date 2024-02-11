1e1e0449c4d4232210192122764902d691e45467
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-11 05:12:03 +0200

    New commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,4 +5,30 @@
 
 class Rectangle:
     """rectangle class"""
-    pass
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, val):
+        if not type(val) is int:
+            raise TypeError("width must be an integer")
+        if val < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = val
+
+    @height.setter
+    def height(self, val):
+        if not type(val) is int:
+            raise TypeError("height must be an integer")
+        if val < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = val

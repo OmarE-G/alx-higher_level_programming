5cbcb3bafe45d3eb5cf46ebdc6cd09688b41954d
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-11 02:02:03 +0200

    New commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,8 +13,9 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
+        print('\n' * self.position[0], end='')
         for i in range(self.__size):
-            print('#' * self.__size)
+            print(' ' * self.position[1] + '#' * self.__size)
         if self.__size == 0:
             print()
 

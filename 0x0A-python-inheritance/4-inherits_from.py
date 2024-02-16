eb93ea6ed031fe99ec9da5b395d0836b74305886
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-17 00:18:06 +0200

    New commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
 
 def inherits_from(obj, a_class):
     """ returns True if the object inherited from class"""
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and (type(obj) is not a_class)

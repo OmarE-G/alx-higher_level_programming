d47edbb731a13d7fabf93643564efc81529dd14b
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-17 00:07:12 +0200

    New commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""
+==MODULE TO CHECK IF INSTANCE OF SAME CLASS or its subclasses==
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """fuction to check if same sub/class"""
+    return isinstance(obj, a_class)

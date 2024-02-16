d7da9bb80fc8f85dbb211ea4cecb2c3a5961baa8
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-17 00:12:37 +0200

    New commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,9 @@ def is_kind_of_class(obj, a_class): module
 
 
 def is_kind_of_class(obj, a_class):
-    """ returns True if the object is an instance of
+    """
+    returns True if the object is an instance of
     a class that inherited (directly or indirectly)
-    from the specified class; otherwise False."""
+    from the specified class; otherwise False.
+    """
     return issubclass(obj, a_class)

c84acd54490a58a5917bc4a9fd06b500ac310aa3
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-17 00:34:47 +0200

    New commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+Base geometry module
+"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    pass

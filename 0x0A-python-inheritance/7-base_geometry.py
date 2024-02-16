6e1ade3911194609090b4719fa3810b8a8d21fdf
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-17 00:39:02 +0200

    New commit

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+Base geometry module
+"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if (type(value) is not int):
+            raise Exception("{} must be an integer".format(name))
+        if value <= 0:
+            raise Exception("{} must be greater than 0".format(name))

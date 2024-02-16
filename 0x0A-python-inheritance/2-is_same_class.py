cfa067fc55caeada45174d4ef86fa0f4acd2d994
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-16 23:54:42 +0200

    New commit

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+==MODULE TO CHECK IF INSTANCE OF SAME CLASS==
+"""
+
+def is_same_class(obj, a_class):
+    """fuction to check if same class"""
+    return isinstance(obj, a_class)
+

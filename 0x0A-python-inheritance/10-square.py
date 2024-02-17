0032852d555c92c00dea5c388df82db5909817cf
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-17 02:31:06 +0200

    one more try

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Square geometry module"""
-R = __import__('9-Rectangle').Rectangle
+"""10-square"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(R):
+class Square(Rectangle):
     """Square class"""
+
     def __init__(self, size):
-        """initialize square"""
-        self.integer_validator("size", size)
+        self.integer_validator('size', size)
         self.__size = size
 
     def area(self):
-        """Area of square"""
         return self.__size ** 2

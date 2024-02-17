2b11fa01e987cf43697901a8bcf7ff22b3b6b775
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-17 02:23:23 +0200

    New commit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,8 +8,10 @@ R = __import__('9-Rectangle').Rectangle
 class Square(R):
     """Square class"""
     def __init__(self, size):
+        """initialize square"""
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        """Area of square"""
         return R.area(self.__size, self.__size)

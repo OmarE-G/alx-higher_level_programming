fdb750b60878b59463f93d34262f1af0809b827e
Omar Essam <omar161157@eng.psu.edu.eg>
2024-03-17 01:58:02 +0200

    New commit

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- show cities of california
 
-SELECT id,name FROM hbtn_0d_usa.cities WHERE state_id=(SELECT id FROM hbtn_0d_usa.states WHERE name = 'California');
+SELECT id,name FROM cities WHERE state_id=(SELECT id FROM states WHERE name = 'California') ORDER BY id;

6df0e9adfbb8029e3dcc2ef40bb756868c8c2530
Omar Essam <omar161157@eng.psu.edu.eg>
2024-03-17 02:52:42 +0200

    New commit

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
     -- comedy
-    SELECT title FROM tv_shows 
+SELECT title FROM tv_shows 
     JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     JOIN tv_genres ON genre_id = tv_genres.id
     WHERE name = 'Comedy'
     ORDER BY title;
-    
+

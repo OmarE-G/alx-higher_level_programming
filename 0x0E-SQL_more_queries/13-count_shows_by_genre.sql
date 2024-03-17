3e90091b6eab9c0051903afa0a4f3373aab6b877
Omar Essam <omar161157@eng.psu.edu.eg>
2024-03-17 02:30:14 +0200

    New commit

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- TV SHOWS
+SELECT name as genre, COUNT(*) as number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;

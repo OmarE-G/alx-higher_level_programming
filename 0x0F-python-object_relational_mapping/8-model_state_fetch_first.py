14b988e2ee6f76e3dfc32b7b9605b41ea7db6c20
Omar Essam <omar161157@eng.psu.edu.eg>
2024-04-20 20:35:22 +0200

    New commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Base State Class"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == '__main__':
+    usr = argv[1]
+    pwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                           .format(usr, pwd, db))
+
+    conn = engine.connect()
+    Sess = sessionmaker(bind=engine)
+    sess = Sess()
+
+    for row in sess.query(State).order_by(State.id).limit(1):
+        print(f"{row.id}: {row.name}")

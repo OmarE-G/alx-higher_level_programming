e945b4b4f6fc3423c300788e2ddf7e2c5f955363
Omar Essam <omar161157@eng.psu.edu.eg>
2024-04-26 03:32:16 +0300

    New commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,12 +2,18 @@
 '''module to find peak'''
 
 
-def find_peak(list_of_integers):
+def find_peak(arr):
     '''find peak in a list of integers'''
-    peak = 'None'
-    for i in range(1, len(list_of_integers) - 1):
-        if (list_of_integers[i] >
-                max(list_of_integers[i-1], list_of_integers[i+1])):
-            peak = list_of_integers[i]
+    peak = None
+    ln = len(arr)
+    if ln > 1 and arr[0] > 1:
+        return arr[0]
+    if ln > 1 and arr[ln-1] > arr[ln-2]:
+        return arr[ln-1]
+
+    for i in range(1, ln - 1):
+        if (arr[i] >
+                max(arr[i-1], arr[i+1])):
+            peak = arr[i]
             break
     return peak

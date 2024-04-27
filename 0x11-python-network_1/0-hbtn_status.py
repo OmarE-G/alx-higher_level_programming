e35c01ed1be15e736e52369b9b0acbed53d9e10b
Omar Essam <omar161157@eng.psu.edu.eg>
2024-04-27 21:21:40 +0300

    New commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     '''get request'''
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         print("Body response:")
-        print(f"\t- type: {type(r.read())}")
-        print(f"\t- content: {r.read()}")
-        print(f"\t- utf8 content: {r.read().decode('utf-8')}")
+        content = r.read()
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")

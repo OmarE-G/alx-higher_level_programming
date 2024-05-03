c84436f8bca1d4ea0f7edf9c774b375cb4487214
Omar Essam <omar161157@eng.psu.edu.eg>
2024-05-04 01:21:52 +0300

    New commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''api request'''
+import requests
+import sys
+
+if __name__ == '__main__':
+    query = 1
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+    data = {'q': query}
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
+    try:
+        print(f"{r.json()['id']} {r.json()['name']}")
+
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
+
+    except KeyError:
+        print("No result")
